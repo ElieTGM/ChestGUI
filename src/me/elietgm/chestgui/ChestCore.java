76dcb9248186f367b52ff33f8595008db81cac10
MrMaurice211 <t_ulu_m@live.com.mx>
2017-06-28 05:47:02 -0700

    Change and reduced a little bit the code! Comment just for let you know

--- src/me/elietgm/chestgui/ChestCore.java
+++ src/me/elietgm/chestgui/ChestCore.java
@@ -2,22 +2,17 @@ package me.elietgm.chestgui;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import me.elietgm.chestgui.api.ItemCreator;
-import me.elietgm.chestgui.api.SkullCreator;
-
 public class ChestCore extends JavaPlugin {
 
-	//4e879827186621dab1f4e52e9d3936e3bd7eb824//
-	
 	private static ChestCore instance;
-	public static ChestCore getInstance() { return instance; }
-	
-	public static ItemCreator getItemCreator() {return new ItemCreator(); }
-	public static SkullCreator getSkullCreator() {return new SkullCreator(); }
-	
+
+	public static ChestCore getInstance() {
+		return instance;
+	}
+
 	@Override
 	public void onEnable() {
-		instance = this;		
+		instance = this;
 	}
-	
+
 }
--- src/me/elietgm/chestgui/api/ItemCreator.java
+++ src/me/elietgm/chestgui/api/ItemCreator.java
@@ -1,5 +1,6 @@
 package me.elietgm.chestgui.api;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.bukkit.Color;
 import org.bukkit.Material;
@@ -12,78 +13,71 @@ import me.elietgm.chestgui.utils.Colorizer;
 
 public class ItemCreator {
 
+	public ItemStack createItem(Material type, int amount, int data, String name) {
+		// ItemStack item = new ItemStack(type, 1, (short) data);
+		// ItemMeta meta = item.getItemMeta();
+		//
+		// item.setAmount(amount);
+		//
+		// meta.setDisplayName(Colorizer.color(name));
+		//
+		// item.setItemMeta(meta);
+		// Repeated code!
+		return createItem(type, amount, data, name, new ArrayList<>());
+	}
+
 	public ItemStack createItem(Material type, int amount, int data, String name, List<String> lore) {
 		ItemStack item = new ItemStack(type, 1, (short) data);
 		ItemMeta meta = item.getItemMeta();
-		
+
 		item.setAmount(amount);
-		
+
 		meta.setDisplayName(Colorizer.color(name));
 		meta.setLore(Colorizer.colorList(lore));
-		
+
 		item.setItemMeta(meta);
-		
+
 		return item;
 	}
-	
+
+	public ItemStack createItemPotion(Material type, int amount, int data, String name) {
+		return createItemPotion(type, amount, data, name, new ArrayList<>());
+	}
+
 	public ItemStack createItemPotion(Material type, int amount, int data, String name, List<String> lore) {
 		ItemStack item = new ItemStack(type, 1, (short) data);
 		PotionMeta meta = (PotionMeta) item.getItemMeta();
-		
+
 		meta.clearCustomEffects();
-		
+
 		item.setAmount(amount);
-		
+
 		meta.setDisplayName(Colorizer.color(name));
 		meta.setLore(Colorizer.colorList(lore));
-		
+
 		item.setItemMeta(meta);
-		
+
 		return item;
 	}
-	
-	public ItemStack createItem(Material type, int amount, int data, String name) {
-		ItemStack item = new ItemStack(type, 1, (short) data);
-		ItemMeta meta = item.getItemMeta();
-		
-		item.setAmount(amount);
-		
-		meta.setDisplayName(Colorizer.color(name));
-		
-		item.setItemMeta(meta);
-		
-		return item;
-	}
-	
+
 	public ItemStack createArmour(Material type, int amount, Color color, String name) {
-		ItemStack item = new ItemStack(type, 1, (short) 0);
-		LeatherArmorMeta meta = (LeatherArmorMeta) item.getItemMeta();
-		
-		item.setAmount(amount);
-		
-		meta.setDisplayName(Colorizer.color(name));
-		
-		meta.setColor(color);
-		
-		item.setItemMeta(meta);
-		
-		return item;
+		return createArmour(type, amount, color, name, new ArrayList<>());
 	}
-	
+
 	public ItemStack createArmour(Material type, int amount, Color color, String name, List<String> lore) {
 		ItemStack item = new ItemStack(type, 1, (short) 0);
 		LeatherArmorMeta meta = (LeatherArmorMeta) item.getItemMeta();
-		
+
 		item.setAmount(amount);
-		
+
 		meta.setDisplayName(Colorizer.color(name));
-		
+
 		meta.setLore(Colorizer.colorList(lore));
-		
+
 		meta.setColor(color);
-		
+
 		item.setItemMeta(meta);
-		
+
 		return item;
 	}
 }
